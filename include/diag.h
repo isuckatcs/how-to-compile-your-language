5a7777abc584172cebfda885569d2d954d1817de
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2026-05-07 02:18:00 +0200

    [Diag] group diagnostics

--- include/diag.h
+++ include/diag.h
@@ -90,47 +90,61 @@ diag(Error, expectedTopLevel, "expected function, struct or trait declaration on
 diag(Error, expectedAtEnd, "expected {} at the end of {}");
 diag(Error, expectedBody, "expected {} body");
 
-diag(Error, unknownType, "the type of '{}' is unknown");
-diag(Error, inferenceError, "{}");
-diag(Error, failedToResolveType, "failed to resolve type '{}'");
-diag(Error, annotationsNeeded, "explicit type annotations are needed to infer the type of '{}'");
-diag(Error, initTyMismatch, "an expression of type '{}' cannot be used to initialize a variable of type '{}'");
-diag(Error, typeParamShadowed, "declaring '{}' shadows outer type parameter");
-diag(Error, typeArgCntMismatch, "type argument count mismatch, expected {}, but received {}");
-diag(Error, expectedOperandTy, "expected '{}' operand");
-diag(Error, invalidCallTy, "calling expression of type '{}'");
-diag(Error, expectedBoolCondition, "expected 'bool' in condition");
-diag(Error, unexpectedTypeParam, "expected value, found type parameter");
-diag(Error, expectedReturnValue, "expected function to return a value");
-diag(Error, expectedReturnValueOnEveryPath, "expected function to return a value on every path");
-diag(Error, rvalueAssignment, "cannot assign to rvalue");
+// symbols
 diag(Error, redeclaration, "redeclaration of '{}'");
 diag(Error, missingSymbol, "symbol '{}' not found");
 diag(Error, lookupInTypeFailed, "failed to find member '{}' in '{}'");
 diag(Error, memberFnLookupFailed, "failed to find member function '{}' in '{}'");
-diag(Error, notInitialized, "'{}' is not initialized");
-diag(Error, cannotBeMutated, "'{}' cannot be mutated");
-diag(Error, fieldAlreadyInitialized, "field '{}' is already initialized");
 diag(Error, noFieldWithName, "'{}' has no field named '{}'");
-diag(Error, fieldNotInitialized, "field '{}' is not initialized");
+diag(Error, cannotAccessMember, "cannot access member of '{}'");
+diag(Error, ambigousMemberFn, "ambigous member function reference");
+
+// types
+diag(Error, unknownType, "the type of '{}' is unknown");
+diag(Error, failedToResolveType, "failed to resolve type '{}'");
+diag(Error, inferenceError, "{}");
+diag(Error, annotationsNeeded, "explicit type annotations are needed to infer the type of '{}'");
+diag(Error, initTyMismatch, "an expression of type '{}' cannot be used to initialize a variable of type '{}'");
+diag(Error, incompatibleAssignment, "expected to assign '{}' but received '{}' instead");
+diag(Error, invalidReturnValue, "cannot return '{}' from a function returning '{}'");
+diag(Error, expectedOperandTy, "expected '{}' operand");
+diag(Error, unaryOperandUnknown, "type of operand to unary operator is unknown");
+diag(Error, binopOperandUnknown, "type of {} to binary operator is unknown");
+diag(Error, binopIncompatibleOperands, "incompatible operands to binary operator ('{}' and '{}')");
+diag(Error, expectedBoolCondition, "expected 'bool' in condition");
+diag(Error, invalidCallTy, "calling expression of type '{}'");
+diag(Error, expectedInstance, "expected an instance of '{}'");
+diag(Error, notStructInstance, "expected struct declaration to instantiate");
+
+// generics
+diag(Error, typeParamShadowed, "declaring '{}' shadows outer type parameter");
+diag(Error, typeArgCntMismatch, "type argument count mismatch, expected {}, but received {}");
+diag(Error, unexpectedTypeParam, "expected value, found type parameter");
+diag(Error, notGeneric, "'{}' is not a generic");
+
+// functions
+diag(Error, wrongArgCount, "wrong number of arguments in function call, expected {}, but received {}");
 diag(Error, expectedMethodCall, "expected to call method");
+diag(Error, classMethodCallOnInstance, "class level methods cannot be called on an instance");
+diag(Error, expectedReturnValue, "expected function to return a value");
+diag(Error, expectedReturnValueOnEveryPath, "expected function to return a value on every path");
+diag(Error, noReturnValue, "expected a return value");
+
+// values
+diag(Error, rvalueAssignment, "cannot assign to rvalue");
+diag(Error, cannotBeMutated, "'{}' cannot be mutated");
+diag(Error, structImmutable, "expected mutable struct instance");
 diag(Error, unexpectedAmpParam, "only parameters can have '&' type");
 diag(Error, ampOutsideArgList, "'&' can only be used to pass arguments to '&' parameters");
 diag(Error, ampWrongCategory, "only mutable lvalues can be passed to '&' parameters");
 diag(Error, mutableAmp, "unexpected 'mut' specifier, a '&' parameter is always mutable");
-diag(Error, unaryOperandUnknown, "type of operand to unary operator is unknown");
-diag(Error, binopOperandUnknown, "type of {} to binary operator is unknown");
-diag(Error, binopIncompatibleOperands, "incompatible operands to binary operator ('{}' and '{}')");
-diag(Error, incompatibleAssignment, "expected to assign '{}' but received '{}' instead");
-diag(Error, noReturnValue, "expected a return value");
-diag(Error, invalidReturnValue, "cannot return '{}' from a function returning '{}'");
-diag(Error, cannotAccessMember, "cannot access member of '{}'");
-diag(Error, ambigousMemberFn, "ambigous member function reference");
-diag(Error, notGeneric, "'{}' is not a generic");
-diag(Error, classMethodCallOnInstance, "class level methods cannot be called on an instance");
-diag(Error, wrongArgCount, "wrong number of arguments in function call, expected {}, but received {}");
 
-// trait
+// init
+diag(Error, notInitialized, "'{}' is not initialized");
+diag(Error, fieldAlreadyInitialized, "field '{}' is already initialized");
+diag(Error, fieldNotInitialized, "field '{}' is not initialized");
+
+// traits
 diag(Error, traitNotImplemented, "'{}' doesn't implement trait '{}'");
 diag(Error, traitMissingMember, "trait '{}' has no member '{}'");
 diag(Error, stricterParamTy, "cannot replace parameter of type '{}' with stricter implementation type '{}'");
@@ -142,11 +156,14 @@ diag(Error, missingTraitFn, "struct '{}' must implement function '{}' from trait
 diag(Error, conflictingTrait, "implementing trait '{}' conflicts with more specific implementation '{}'");
 diag(Error, missingRequirement, "implementing trait '{}' requires implementing trait '{}'");
 
-// struct
-diag(Error, expectedInstance, "expected an instance of '{}'");
-diag(Error, structImmutable, "expected mutable struct instance");
-diag(Error, notStructInstance, "expected struct declaration to instantiate");
+// structs
 diag(Error, selfContainingStruct, "struct '{}' contains itself");
+
+// self
+diag(Error, selfTyNotAllowed, "'Self' is only allowed inside structs and traits");
+diag(Error, selfParamNotAllowed, "'self' parameter is only allowed in methods");
+diag(Error, selfWrongPosition, "'self' can only be the first parameter");
+diag(Error, selfWrongType, "the type of 'self' must reference 'Self'");
 
 // main
 diag(Error, wrongMainReturnTy, "'main' function is expected to return 'unit'");
@@ -154,14 +171,8 @@ diag(Error, wrongMainArgCount, "'main' function is expected to take no arguments
 diag(Error, mainIsGeneric, "'main' function cannot be generic");
 diag(Error, mainNotFound, "'main' function not found");
 
-// builtin
+// builtins
 diag(Error, reservedPrintf, "'printf' is a reserved function name and cannot be used for user-defined functions");
-
-// self
-diag(Error, selfTyNotAllowed, "'Self' is only allowed inside structs and traits");
-diag(Error, selfParamNotAllowed, "'self' parameter is only allowed in methods");
-diag(Error, selfWrongPosition, "'self' can only be the first parameter");
-diag(Error, selfWrongType, "the type of 'self' must reference 'Self'");
 // clang-format on
 }; // namespace err
 
