ce66dd3603f61097ba2b8bd3960bbdc12198eb00
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2026-05-24 03:01:42 +0200

    [Test] test the garbage collector thoroughly

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -1,7 +1,12 @@
 import os
+import shutil
 import lit.formats
 
 config.name = "how-to-compile-your-language"
 config.test_format = lit.formats.ShTest(False if os.name == 'nt' else True)
 
 config.suffixes = ['.yl']
+
+config.substitutions += [
+    ("%valgrind", "valgrind --error-exitcode=1" if shutil.which("valgrind") else "")
+]
