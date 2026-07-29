50361396860e8ce00441f53de5583adee04f7c3f
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2026-07-29 15:35:43 +0200

    [Sema] detect ambigous member functions in generic traits correctly

--- src/sema.cpp
+++ src/sema.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <map>
@@ -554,15 +555,19 @@ Sema::lookupAssociatedDecls(std::string identifier,
 
     auto constraints = typeMgr.getDirectConformance(type);
     std::deque<res::TraitType *> traits(constraints.begin(), constraints.end());
-    std::set<res::NamedDecl *> seen;
+    std::vector<res::TraitType *> seen;
 
     while (!traits.empty()) {
       res::TraitType *trait = traits.front();
       traits.pop_front();
 
+      auto pred = [&](res::TraitType *t) { return typeMgr.eq(t, trait); };
+      if (std::find_if(seen.begin(), seen.end(), pred) != seen.end())
+        continue;
+      seen.emplace_back(trait);
+
       for (auto &&decl : trait->getDecl()->lookupDirect(identifier))
-        if (seen.emplace(decl).second)
-          candidates.emplace_back(decl, typeMgr.extractSubstitutionFrom(trait));
+        candidates.emplace_back(decl, typeMgr.extractSubstitutionFrom(trait));
 
       for (auto &&parent : typeMgr.getDirectConformance(trait))
         traits.emplace_back(parent);
